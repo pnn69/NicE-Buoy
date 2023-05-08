b64e1790477b8afb10e1ea9963f289de5ce806a7
pnn69 <pnn_69@hotmail.com>
2023-05-08 21:00:00 +0200

    Some info added

--- NicE-Buoy/src/main.cpp
+++ NicE-Buoy/src/main.cpp
@@ -1,9 +1,18 @@
+/*
+https://github.com/sandeepmistry/arduino-LoRa/blob/master/examples/LoRaDuplex/LoRaDuplex.ino
+http://www.lilygo.cn/prod_view.aspx?TypeId=50003&Id=1130&FId=t3:50003:3
+https://github.com/Xinyuan-LilyGO/TTGO-LoRa-Series
+https://github.com/Xinyuan-LilyGO/LilyGo-LoRa-Series/blob/master/schematic/T3_V1.6.1.pdf
+433MHz is SX1278
+*/
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
