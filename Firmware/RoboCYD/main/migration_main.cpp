a179f04570995b2315d77071e58d4da3d822316f
Peter de Nijs <pnn69pnn@gmail.com>
2026-09-26 12:41:11 +0200

    Create initial RoboCYD ESP-IDF 5.3 project

---
+++ Firmware/RoboCYD/main/migration_main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern "C" void app_main(void)
+{
+    printf("\n");
+    printf("====================================\n");
+    printf(" RoboCYD ESP-IDF migration build\n");
+    printf("====================================\n");
+    printf("Native ESP-IDF startup successful.\n");
+}
